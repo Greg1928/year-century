ac2d509097acf0824e374eb9756345ed78f0ab80
Greg1928 <gregoriosortino@protonmail.com>
2022-07-27 22:50:49 +0200

    fixes

--- main.js
+++ main.js
@@ -1,34 +1,37 @@
+// FUNCTION
+function yearToCentury(){
+    if(syear.slice(index, syear.length) === "01" || syear.slice(index, syear.length) !== "00"){
+        firstHalf = syear.slice(0,index);
+        alert(parseInt(firstHalf) + 1 + "° secolo");
+        window.location.reload();
+}else {
+    alert(syear.slice(0,index)+ "° secolo");
+    window.location.reload();
+    }
+}
 
+// VARIABLES
 let year;
-let middle;
-let first;
+let firstHalf;
+let index;
+let syear
 
+// USER INPUT VALIDATE
 do{
     year = Number(prompt("Anno:"));
 }while(year > 9999 || !parseInt(year));
 
-if(year >= 1 && year <= 100){
-    alert("1° secolo")
-}else if(year >= 101 && year <= 1000){
-    let syear = year.toString();
-    if(syear.slice(1, syear.length) === "01" || syear.slice(1, syear.length) !== "00"){
-        first = syear.slice(0,1);
-        alert(parseInt(first) + 1 + "° secolo");
-        window.location.reload();
-    }else {
-        alert(syear.slice(0,1)+ "° secolo");
-        window.location.reload();
-    }
-}else if(year >= 1001 && year <= 10000){
-    let syear = year.toString();
-    if(syear.slice(2, syear.length) === "01" || syear.slice(2, syear.length) !== "00"){
-        first = syear.slice(0,2);
-        alert(parseInt(first) + 1 + "° secolo");
-        window.location.reload();
-    }else {
-        alert(syear.slice(0,2)+ "° secolo");
-        window.location.reload();
-    }
-}else{
-    window.location.reload();
+syear = year.toString();
+
+// SET INDEX
+if(syear.length === 3){
+    index = 1;
+}else if (syear.length === 4){
+    index = 2;
 }
+
+// MAIN
+if(year >= 1 && year <= 100){
+    alert("1° secolo");
+    window.location.reload();
+}else yearToCentury();
